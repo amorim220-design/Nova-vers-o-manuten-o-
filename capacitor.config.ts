5a954f34b96e86eb784acab7585321eef093d5ff
amorim220-design <amorim220@gmail.com>
2026-01-19 18:42:22 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,10 @@
-
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.manutencao.app',
   appName: 'Manutenção de Apartamentos',
   webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  },
+  bundledWebRuntime: false,
   plugins: {
     LocalNotifications: {}
   }
