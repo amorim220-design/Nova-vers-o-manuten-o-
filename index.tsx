9e7ac3f088f5c8d79dee74d8a3b1c19deead33cd
amorim220-design <amorim220@gmail.com>
2026-01-19 16:16:38 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,6 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -10,10 +8,9 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
